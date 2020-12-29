7010713943f13bc3f7ca9f0e4076008d2b6e3137
Jas-F <jasminer.franklin@gmail.com>
2020-12-29 11:13:49 -0500

    add 10-5 to local storage

--- index.js
+++ index.js
@@ -25,6 +25,15 @@ localStorage.setItem(id,inputValue)
 });
 
 $('#9 .description').val(localStorage.getItem('9'))
+$('#10 .description').val(localStorage.getItem('10'))
+$('#11 .description').val(localStorage.getItem('11'))
+$('#12 .description').val(localStorage.getItem('12'))
+$('#1 .description').val(localStorage.getItem('1'))
+$('#2 .description').val(localStorage.getItem('2'))
+$('#3 .description').val(localStorage.getItem('3'))
+$('#4 .description').val(localStorage.getItem('4'))
+$('#5add .description').val(localStorage.getItem('5'))
+
 
 dateBtn.addEventListener('click', ()=> {
     let today= new Date();
