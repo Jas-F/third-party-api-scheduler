daa30d9b9517dd269639b3605a641f588cde0d85
Jas-F <jasminer.franklin@gmail.com>
2020-12-31 11:04:24 -0500

    convert to military time

--- index.js
+++ index.js
@@ -20,20 +20,17 @@ $('#9 .description').val(localStorage.getItem('9'))
 $('#10 .description').val(localStorage.getItem('10'))
 $('#11 .description').val(localStorage.getItem('11'))
 $('#12 .description').val(localStorage.getItem('12'))
-$('#1 .description').val(localStorage.getItem('1'))
-$('#2 .description').val(localStorage.getItem('2'))
-$('#3 .description').val(localStorage.getItem('3'))
-$('#4 .description').val(localStorage.getItem('4'))
-$('#5 .description').val(localStorage.getItem('5'))
+$('#13 .description').val(localStorage.getItem('13'))
+$('#14 .description').val(localStorage.getItem('14'))
+$('#15 .description').val(localStorage.getItem('15'))
+$('#16 .description').val(localStorage.getItem('16'))
+$('#17 .description').val(localStorage.getItem('17'))
 
 // change text area background to distinguish between past present or future
 // compare current time to time block
 // if time block is less than current time then add class of past
 // repeat for present and future
 
-// for(var i in textarea) {
-//     if 
-// }
 function updateHour () {
 $('.time-block').each(function(){
 let timeBlock= $(this).attr('id')
