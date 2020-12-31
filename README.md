24176eb0eddc4bc223edbaaff6d1be0effe9256f
Jas-F <jasminer.franklin@gmail.com>
2020-12-30 21:36:38 -0500

    change id names

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
 # third-party-api-scheduler
 Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery. Use a library like Moment.js to work with dates and times.
+
+===========
+![Image](website4.png)
+
+<br>
+
+## Utilize JQuery
+
+```
+ timerInterval = setInterval(function() {
+        timeLeft--;
+        quizTimer.textContent = "Time Remaining: " + timeRemaining;
+
+        if(timeRemaining === 0) {
+            clearInterval(timerInterval);
+            showScore();
+        }
+        }, 1000);
+        quiz.style = "block";
+    }
+```
+<br>
+
+## Feature CSS
+
+```
+button{
+    background-color: crimson;
+    text-align: center;
+    font-family: sans-serif;
+    color: black;
+}
+```
+<br>
+
+## Multiple Choice Quiz
+
+```
+    question6: "Who Made This Quiz",
+    a:  "Jasmine Franklin The Coder",
+    b:  "Jasmine Franklin The Coder",
+    c:  "Jasmine Franklin The Coder",
+    d:  "Jasmine Franklin The Coder",
+    answer: "Jasmine Franklin The Coder"}
+]
+```
+
+<br>
+
+## Built With
+
+* [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+* [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+* [Javascript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+* [Bootstrap](https://getbootstrap.com/)
+
+## Deployed Link
+
+* [See Live Site](https://jas-f.github.io/Web-api-code-quiz/)
+
+## License
+
+This project is licensed under the MIT License 
+
+See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
+
+## Prerequisites
+
+Git hub,
+Git lab,
+Git bash,
+Visual studio,
+Google chrome,
+Bootstrap,
+JavaScript
+## Authors
+
+**Jasmine Franklin** 
+
+- [Link to Portfolio Site](https://jas-f.github.io/responsive-portfolio/)
+- [Link to Github](https://github.com/)
+- [Link to LinkedIn](https://www.linkedin.com/in/jasmine-franklin-8b08ba121)
+
+<p>&copy; UC Berkeley Extension Bootcamp.</p>
--- index.js
+++ index.js
@@ -1,28 +1,39 @@
-
 $('#currentDay').text(moment().format('dddd') +","+(moment().format('MMMM Do YYYY, h:mm a')));
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
-let saveBtn= $('.saveBtn');
-let output= $('#currentDay');
-let dateBtn= $('.dateBtn');
+// let saveBtn= $('.saveBtn');
+// let time=$('.row');
+
+// saveBtn.on('click', function (){
+// let inputValue=$(this).siblings('.description').val()
+// let id=$(this).parent().attr('id')
+// localStorage.setItem(id,inputValue)
+// });
+
+// $('#9 .description').val(localStorage.getItem('9'))
+// $('#10 .description').val(localStorage.getItem('10'))
+// $('#11 .description').val(localStorage.getItem('11'))
+// $('#12 .description').val(localStorage.getItem('12'))
+// $('#1 .description').val(localStorage.getItem('1'))
+// $('#2 .description').val(localStorage.getItem('2'))
+// $('#3 .description').val(localStorage.getItem('3'))
+// $('#4 .description').val(localStorage.getItem('4'))
+// $('#5 .description').val(localStorage.getItem('5'))
 
 
+// time (function () {if id > currentTime
+// this.add(.past)
+// })
 
-saveBtn.on('click', function (){
-let inputValue=$(this).siblings('.description').val()
-let id=$(this).parent().attr('id')
-localStorage.setItem(id,inputValue)
-});
-
-$('#9 .description').val(localStorage.getItem('9'))
-$('#10 .description').val(localStorage.getItem('10'))
-$('#11 .description').val(localStorage.getItem('11'))
-$('#12 .description').val(localStorage.getItem('12'))
-$('#1 .description').val(localStorage.getItem('1'))
-$('#2 .description').val(localStorage.getItem('2'))
-$('#3 .description').val(localStorage.getItem('3'))
-$('#4 .description').val(localStorage.getItem('4'))
-$('#5 .description').val(localStorage.getItem('5'))
-
-})
+// distinguish past present and future
+// compare present time with each other
+// compare x time to present time to determine if past or future
+// name out present time to variable to compare
+// let current time = moment.hour
+let currentTime=moment().hour();
+console.log(currentTime)
+// this will show the current time in your time zone (21)
+// compare time block hours with 21
+// if time block is less than 21 then past
+// if time block is greater than 21 then future
